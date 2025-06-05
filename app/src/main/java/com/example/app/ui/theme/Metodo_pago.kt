9d704b4b8cfb4eeb7c315d148db739e1e8e84ea6
Nicolee2702 <nicoleeraychellvillacorta@gmail.com>
2025-06-05 12:39:34 -0500

    Merge remoto con cambios locales

---
+++ app/src/main/java/com/example/app/ui/theme/Metodo_pago.kt
@@ -0,0 +1,30 @@
+package com.example.app.ui.theme
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper // Nicolee
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.app.ui.theme.LoginActivity
+import com.example.nuevo_proyecto.R
+
+class Metodo_pago : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Asegúrate de que 'activity_login' sea el nombre de tu archivo XML de layout (ej. activity_login.xml)
+        setContentView(R.layout.prueba_perfil)
+
+        val btnIrACar = findViewById<Button>(R.id.button)
+
+        // Acción al hacer clic para ir a RegisterActivity
+        btnIrACar.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
