d99c1409dd05d4ae07380c6c010ac26b36801bea
Lucero0520 <luceritomejiatapia736@gmail.com>
2025-06-06 14:51:40 -0500

    lucero nuevo 06 de junio

--- app/src/main/java/com/example/RegisterActivity.kt
+++ app/src/main/java/com/example/RegisterActivity.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:app/src/main/java/com/example/RegisterActivity.kt
-package com.example
-========
-package com.example.app.ui.theme
->>>>>>>> 3a6784df1bc8571ba5502db96085dc9049c8e43d:app/src/main/java/com/example/app/ui/theme/RegisterActivity.kt
+package com.example // Asegúrate de que el paquete sea correcto
 
 import android.os.Bundle
 import android.widget.Button
@@ -12,10 +8,10 @@ import com.example.nuevo_proyecto.R
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register) // usa el nombre de tu archivo XML (sin .xml)
+        setContentView(R.layout.register) // Vincula con register.xml
         val btnIrARegistro = findViewById<Button>(R.id.btnRegistro)
-        // Referencia al botón (asegúrate de que tenga el ID en el XML)
+        btnIrARegistro.setOnClickListener {
 
-
+        }
     }
 }
--- app/src/main/java/com/example/app/ui/theme/connection.kt
+++
@@ -1,4 +0,0 @@
-package com.example.app.ui.theme
-
-class connection {
-}
