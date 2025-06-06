693ec758cf97d28b31d78fb75db09e840bea115f
Lucero0520 <luceritomejiatapia736@gmail.com>
2025-06-06 14:43:30 -0500

    lucero nuevo 42

--- app/src/main/java/com/example/ProductActivity.kt
+++ app/src/main/java/com/example/ProductActivity.kt
@@ -1,13 +1,21 @@
 package com.example
+
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nuevo_proyecto.R
 
 class ProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.product) // Vincula con producto.xml
+        setContentView(R.layout.product) // Vincula con product.xml
 
+        // Encuentra el botón en el diseño
+        val houseButton = findViewById<ImageButton>(R.id.houseinicio)
 
+        // Configurar el botón para cerrar ProductActivity y volver a MainActivity
+        houseButton.setOnClickListener {
+            finish() // Cierra ProductActivity y vuelve a MainActivity
+        }
     }
 }
